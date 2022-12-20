dfe8d071a915909b13d182873fececcf5fb8e991
yash pitroda <yashpitroda06@gmail.com>
2022-12-21 03:21:25 +0530

    added client delete

--- server/server.py
+++ server/server.py
@@ -1,29 +1,18 @@
 
 from flask_cors import CORS #imp
-from flask_mysqldb import MySQL
 from flask import Flask,request,jsonify
 
 import util
 
-
-
 prt = 9000
 app = Flask(__name__)
 CORS(app)
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'yash2020'
-# app.config['MYSQL_DB'] = 'cashbook'
 
-# mysql=MySQL(app)
-# print(mysql)
 
 
 @app.route('/clientadd',methods=['POST'])
 def clientadd():
-    
     value=request.get_json()
-    # print(value)
     requird=['cname','cmobileno']
     if not all(key in value for key in requird):
          return {'error':'cname or cmobile will be None or null','status':'fail'},400
@@ -32,32 +21,38 @@ def clientadd():
     cname=value['cname']
     # print(cname)
 
-    try:
-        # query = f"SELECT * FROM client WHERE cmobileno={cmobileno}" #find client in db
-        # cursor.execute(query)
-        # fatchData=cursor.fetchone() #user is exist or not
-        fetchdata=util.findclient(cmobileno) #find client in db
-        print(fetchdata) 
-        if(fetchdata==None):
-            #insert in db
-            # query=f"INSERT INTO client(cname,cmobileno) values('{cname}','{cmobileno}')"
-            # print(query)
-            # cursor.execute(query)
-            # mysql.connection.commit()
-            status=util.insertclient(cname,cmobileno)
-            return {'status':status},200
-        return {'status':'Client already exists'},200
-    except Exception as e:
-        print(e)
-        return {'error':e,'status':'fail'},400
+    fetchdata=util.findclient(cmobileno) #find client in db 
+    print(fetchdata) 
+    
+    if(fetchdata==None):
+        # insert in db
+        status=util.insertclient(cname,cmobileno)
+        return {'status':status},200
+    
+    return {'status':fetchdata},200 #user is already exisit so retrun user
+   
         
-# @app.route('/clientdelete',method=["POST"])
-# def clientdelete():
-#     cursor = mysql.connection.cursor()
+@app.route('/clientdelete/',methods=['POST'])
+def clientdelete():
+    value=request.get_json()
+    requird=['cmobileno']
+    if not all(key in value for key in requird):
+         return {'error':'cmobile will be None or null','status':'fail'},400
+        
+    cmobileno=value['cmobileno']
+   
+    status=util.deleteclient(cmobileno) #find client in db 
+    print(status) 
+
+    return {'status':status},200 #user delted
+   
+
+
+
     
    
 
     
-    # return jsonify('print suceess')
+
 
 app.run(debug=True,port=prt,host='0.0.0.0')
--- server/util.py
+++ server/util.py
@@ -22,17 +22,37 @@ cursor = db.cursor()
 #     return fatchData
 
 def findclient(cmobileno):
-    
-    query = f"SELECT * FROM client WHERE cmobileno={cmobileno}"
-    cursor.execute(query)
-    fatchData=cursor.fetchone() #user is exist or not
-    print("in utill",fatchData) 
-    return fatchData
+    try:
+        # query = f"SELECT * FROM client WHERE cmobileno={cmobileno}"
+        query = f"SELECT * FROM client WHERE cmobileno={cmobileno}"
+        cursor.execute(query)
+        fatchData=cursor.fetchone() #user is exist or not
+        print("in utill",fatchData) 
+        return fatchData
+    except Exception as e:
+        print(e)
+        return "database error"
 
 
 def insertclient(cname,cmobileno):
-    query=f"INSERT INTO client(cname,cmobileno) values('{cname}','{cmobileno}')"
-    print(query)
-    cursor.execute(query)
-    mysql.connection.commit()
-    return "new clint added"
+    try:
+        query=f"INSERT INTO client(cname,cmobileno) values('{cname}','{cmobileno}')"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "new clint added"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def deleteclient(cmobileno):
+    try:
+        query=f"delete from client where cmobileno='{cmobileno}'"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "delete client success"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
