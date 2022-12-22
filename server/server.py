6254653cb13b709a9aef5cebc08183165f7d2b88
yash pitroda <yashpitroda06@gmail.com>
2022-12-22 14:47:58 +0530

    add - insert,delete, fetch in paid/payable table

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 
 from flask_cors import CORS #imp
 from flask import Flask,request,jsonify
+import json
 
 import util
 
@@ -32,7 +33,7 @@ def clientadd():
     return {'status':fetchdata},200 #user is already exisit so retrun user
    
         
-@app.route('/clientdelete/',methods=['POST'])
+@app.route('/clientdelete',methods=['POST'])
 def clientdelete():
     value=request.get_json()
     requird=['cmobileno']
@@ -45,14 +46,197 @@ def clientdelete():
     print(status) 
 
     return {'status':status},200 #user delted
+
+@app.route('/additeminpaidtable',methods=['POST'])
+def additeminpaidtable():
+    """body
+    {
+    "cmobileno": "1234",
+    "paisbill": "1",
+    "padescription": "flsfjfsjf",
+    "paamount": "1000.12",
+    "paymentmode" :"cash"
+    }
+    """
+    value=request.get_json()
+    requird=["cmobileno","paisbill","padescription","paamount","paymentmode"]
+    if not all(key in value for key in requird):
+         return {'error':'cmobile will be None or null','status':'fail'},400
+        
+    cmobileno=value['cmobileno'] #string
+    paisbill=int(value['paisbill']) #int
+    padescription=value['padescription'] #stirng
+    paamount=float(value['paamount']) #doble
+    paymentmode=value['paymentmode'] #stirng
    
+    status=util.addItemInPaidTable(cmobileno,padescription,paisbill,paymentmode,paamount) #find client in db 
+    print(status) 
 
+    return {'status':status},200 #user delted
 
+@app.route('/additeminpayabletable',methods=['POST'])
+def additeminpayabletable():
+    """body
+    {
+    "cmobileno": "1234",
+    "pyisbill": "1",
+    "pydescription": "flsfjfsjf",
+    "pyamount": "1000.12"
+    }
+    """
+    value=request.get_json()
+    requird=["cmobileno","pyisbill","pydescription","pyamount"]
+    if not all(key in value for key in requird):
+         return {'error':'cmobile will be None or null','status':'fail'},400
+        
+    cmobileno=value['cmobileno'] #string
+    pyisbill=int(value['pyisbill']) #int
+    pydescription=value['pydescription'] #stirng
+    pyamount=float(value['pyamount']) #doble
 
-    
    
+    status=util.addItemInPayableTable(cmobileno,pydescription,pyisbill,pyamount) #find client in db 
+    print(status) 
 
+    return {'status':status},200 #user delted
+
+@app.route('/deleteiteminpayabletable',methods=['POST'])
+def deleteiteminpayabletable():
+    """body
+    {
+    "sno":"1"
+    }
+    """
+    value=request.get_json()
+    requird=["sno"]
+    if not all(key in value for key in requird):
+         return {'error':'cmobile will be None or null','status':'fail'},400
+        
+    sno=int(value['sno']) #string
+    status=util.deleteItemInPayableTable(sno) 
+    print(status) 
+
+    return {'status':status},200 
+
+@app.route('/deleteiteminpaidtable',methods=['POST'])
+def deleteiteminpaidtable():
+    """body
+    {
+    "sno":"1"
+    }
+    """
+    value=request.get_json()
+    requird=["sno"]
+    if not all(key in value for key in requird):
+         return {'error':'cmobile will be None or null','status':'fail'},400
+        
+    sno=int(value['sno']) #string
+    status=util.deleteItemInPaidTable(sno) 
+    print(status) 
+
+    return {'status':status},200 
+
+@app.route('/fetchallpaidtable',methods=['POST'])
+def fetchallpaidtable():
+    """body
     
+    """
+    result=util.fetchAllItemInPaidTable() #result hold list of tupple -- [(),(),()]
+    """
+    {
+    "datalist": [
+        {
+        "cmobileno": "1234",
+        "padate": "Thu, 22 Dec 2022 11:42:47 GMT",
+        "padescription": "flsfjfsjf",
+        "paisbill": 1,
+        "pamount": 1000.12,
+        "paymentmode": "cash",
+        "sno": 1
+        },
+        {
+        "cmobileno": "1234",
+        "padate": "Thu, 22 Dec 2022 11:42:47 GMT",
+        "padescription": "flsfjfsjf",
+        "paisbill": 1,
+        "pamount": 1000.12,
+        "paymentmode": "cash",
+        "sno": 3
+        }
+    ]
+    }
+    """
+    # print(result) 
+    # print(jsonify(result)) 
+    #dumps : is used to convert all row into list form:[] 
+    # print(json.dumps(result,default=str)) #default=str mean covert all paramiter to stirng 
+    # print(json.decode(response.body)); -- frented side
+    # listOfPaidTableData=json.dumps(result,default=str)
+    
+    PaidTableDataList=[]
+    for i in result:
+        sno,cmobileno,padate,paisbill,padescription,pamount,paymentmode=i
+        temp={
+            "sno":sno,
+            "cmobileno":cmobileno,
+            "padate":padate,
+            "paisbill":paisbill,
+            "padescription":padescription,
+            "pamount":pamount,
+            "paymentmode":paymentmode,
+        }
+        PaidTableDataList.append(temp)
+    print(PaidTableDataList)
+    return {'datalist': PaidTableDataList},200 
+
+
+@app.route('/fetchallpayabletable',methods=['POST'])
+def fetchallpayabletable():
+    """body
+    
+    """
+    result=util.fetchAllItemInPayableTable() #result hold list of tupple -- [(),(),()]
+    """
+    {
+    "datalist": 
+        [
+            {
+            "cmobileno": "1234",
+            "padate": "Thu, 22 Dec 2022 14:39:16 GMT",
+            "padescription": "flsfjfsjf",
+            "paisbill": 1,
+            "pamount": 7889.12,
+            "sno": 3
+            },
+            {
+            "cmobileno": "1234",
+            "padate": "Thu, 22 Dec 2022 14:39:28 GMT",
+            "padescription": "flsfjfsjf",
+            "paisbill": 1,
+            "pamount": 3422.12,
+            "sno": 4
+            },
+        ]
+    }
+    """
+    PayableTableDataList=[]
+    for i in result:
+        sno,cmobileno,pydate,pyisbill,pydescription,pyamount=i
+        temp={
+            "sno":sno,
+            "cmobileno":cmobileno,
+            "pydate":pydate,
+            "pyisbill":pyisbill,
+            "pydescription":pydescription,
+            "pyamount":pyamount,
+        }
+        PayableTableDataList.append(temp)
+    print(PayableTableDataList)
+    return {'datalist': PayableTableDataList},200 
+
+
+
+
 
 
 app.run(debug=True,port=prt,host='0.0.0.0')
--- server/util.py
+++ server/util.py
@@ -56,3 +56,68 @@ def deleteclient(cmobileno):
         print(e)
         return "database error"
     
+def addItemInPaidTable(cmobileno,padescription,paisbill,paymentmode,paamount):
+    try:
+        query=f"INSERT INTO paid_table (cmobileno,paisbill,padescription,paamount,paymentmode) VALUES ('{cmobileno}', '{paisbill}', '{padescription}', '{paamount}', '{paymentmode}');"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "added in paid_table success"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def addItemInPayableTable(cmobileno,pydescription,pyisbill,pyamount):
+    try:
+        query=f"INSERT INTO payable_table (cmobileno,pyisbill,pydescription,pyamount) VALUES ('{cmobileno}', '{pyisbill}', '{pydescription}', '{pyamount}');"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "added in payable_table success"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def deleteItemInPayableTable(sno):
+    try:
+        query=f"delete from payable_table where sno='{sno}'"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "delete item in payable_table success"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def deleteItemInPaidTable(sno):
+    try:
+        query=f"delete from paid_table where sno='{sno}'"
+        print(query)
+        cursor.execute(query)
+        db.commit()
+        return "delete item in paid_table success"
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def fetchAllItemInPaidTable():
+    try:
+        query=f"SELECT * FROM paid_table"
+        print(query)
+        cursor.execute(query)
+        fetchdata= cursor.fetchall()
+        return fetchdata
+    except Exception as e:
+        print(e)
+        return "database error"
+    
+def fetchAllItemInPayableTable():
+    try:
+        query=f"SELECT * FROM payable_table"
+        print(query)
+        cursor.execute(query)
+        fetchdata= cursor.fetchall()
+        return fetchdata
+    except Exception as e:
+        print(e)
+        return "database error"
